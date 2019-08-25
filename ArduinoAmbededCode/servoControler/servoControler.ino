3086285f5c2a596f371da99e486fb01a104b9e5e
gui4d <g.ducombs@free.fr>
2019-08-25 14:54:32 +0200

    add servo personalized angle and arduino test

--- ArduinoAmbededCode/servoControler/servoControler.ino
+++ ArduinoAmbededCode/servoControler/servoControler.ino
@@ -6,25 +6,26 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define MAX_PULSE_WIDTH 2350
 #define DEFAULT_PULSE_WIDTH 1500
 #define FREQUENCY 50
-#define NUMBER_SERVO 16
+#define NUMBER_SERVO 10
 
 
 unsigned long timeStampList[NUMBER_SERVO];
+unsigned int Min_Pulse_Width[NUMBER_SERVO];
+unsigned int MAx_Pulse_width[NUMBER_SERVO];
 
-int pulseWidth(int angle){
+int pulseWidth(int angle, int servo){
   int pulse_wide, analog_value;
-  pulse_wide = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  pulse_wide = map(angle, 0, 180, Min_Pulse_Width[servo], MAx_Pulse_width[servo]);
   analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
-  //Serial.println(analog_value);
   return analog_value;
 }
 
 void openServo(int servonumber){
-  pwm.setPWM(servonumber, 0, pulseWidth(0)); 
+  pwm.setPWM(servonumber, 0, pulseWidth(0,servonumber)); 
 }
 
 void closeServo(int servonumber){
-  pwm.setPWM(servonumber, 0, pulseWidth(180)); 
+  pwm.setPWM(servonumber, 0, pulseWidth(180,servonumber)); 
 }
 
 void UpdateServoState()
@@ -84,16 +85,37 @@ void setup() {
   Serial.begin(9600);
   pwm.begin();
   pwm.setPWMFreq(FREQUENCY);
-  int i; 
+  int i;
+   
   for( i = 0 ; i < 10 ; i++){
-    timeStampList[i]=0;
-    closeServo(i);
+    MAx_Pulse_width[i] = MAX_PULSE_WIDTH; //to do one by one after 
+    Min_Pulse_Width[i] = MIN_PULSE_WIDTH;
+    timeStampList[i]=0;   
   } 
+  MAx_Pulse_width[0]=2400;
+  MAx_Pulse_width[1]=2150;
+  MAx_Pulse_width[2]=2000;
+  MAx_Pulse_width[3]=2350;
+  MAx_Pulse_width[4]=2350;
+  MAx_Pulse_width[5]=1900;
+  MAx_Pulse_width[6]=2350;
+  MAx_Pulse_width[7]=2500;
+  Min_Pulse_Width[0]=640;
+  Min_Pulse_Width[1]=640;
+  Min_Pulse_Width[2]=640;
+  Min_Pulse_Width[3]=640;
+  Min_Pulse_Width[4]=640;
+  Min_Pulse_Width[5]=640;
+  Min_Pulse_Width[6]=640;
+  Min_Pulse_Width[7]=640;
+for( i = 0 ; i < 10 ; i++)
+{
+  closeServo(i);
+  delay(300);
+  }
 }
 
 
 void loop() {
-  UpdateServoState();
-  //Serial.println(millis());
-  
+  UpdateServoState();  
 }
